a4940ebd90010436342b9b047d5703fdffe4e515
EdisonSegovia <sego10@hotmail.com>
2022-10-22 11:37:47 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoSunshineBouquet/Controllers/GradoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoSunshineBouquet.Models;
+
+namespace ProyectoSunshineBouquet.Controllers
+{
+    public class GradoesController : Controller
+    {
+        private DBSBProductoEntities1 db = new DBSBProductoEntities1();
+
+        // GET: Gradoes
+        public ActionResult Index()
+        {
+            return View(db.Grado.ToList());
+        }
+
+        // GET: Gradoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grado grado = db.Grado.Find(id);
+            if (grado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grado);
+        }
+
+        // GET: Gradoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gradoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GradoId,GradoNombre")] Grado grado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Grado.Add(grado);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(grado);
+        }
+
+        // GET: Gradoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grado grado = db.Grado.Find(id);
+            if (grado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grado);
+        }
+
+        // POST: Gradoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GradoId,GradoNombre")] Grado grado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(grado).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(grado);
+        }
+
+        // GET: Gradoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grado grado = db.Grado.Find(id);
+            if (grado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grado);
+        }
+
+        // POST: Gradoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Grado grado = db.Grado.Find(id);
+            db.Grado.Remove(grado);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
